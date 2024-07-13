47b041d2d2bf7f06bea35d3c091f74bb1e07bc8b
hatoo <hato2000@gmail.com>
2024-07-13 22:25:55 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -97,14 +97,14 @@ impl<C: Borrow<rcgen::CertifiedKey> + Send + Sync + 'static> MitmProxy<C> {
         service: S,
     ) -> Result<impl Future<Output = ()>, std::io::Error>
     where
-        B: Body + Send + 'static,
-        <B as Body>::Data: Send,
-        <B as Body>::Error: Into<Box<(dyn std::error::Error + std::marker::Send + Sync + 'static)>>,
+        B: Body<Data = Bytes, Error = E> + Send + Sync + 'static,
         E: std::error::Error + Send + Sync + 'static,
         S: Fn(Request<Incoming>) -> F + Send + Sync + Clone + 'static,
         F: Future<Output = Result<Response<B>, E>> + Send,
     {
         let listener = TcpListener::bind(addr).await?;
+
+        let proxy = Arc::new(self);
 
         Ok(async move {
             loop {
@@ -114,13 +114,14 @@ impl<C: Borrow<rcgen::CertifiedKey> + Send + Sync + 'static> MitmProxy<C> {
 
                 let service = service.clone();
 
+                let proxy = proxy.clone();
                 tokio::spawn(async move {
                     if let Err(err) = server::conn::http1::Builder::new()
                         .preserve_header_case(true)
                         .title_case_headers(true)
                         .serve_connection(
                             TokioIo::new(stream),
-                            service_fn(|req| Self::proxy(req, service.clone())),
+                            service_fn(|req| Self::proxy(proxy.clone(), req, service.clone())),
                         )
                         .with_upgrades()
                         .await
@@ -132,12 +133,109 @@ impl<C: Borrow<rcgen::CertifiedKey> + Send + Sync + 'static> MitmProxy<C> {
         })
     }
 
-    async fn proxy<S, B, E, F>(req: Request<Incoming>, service: S) -> Result<Response<B>, E>
+    async fn proxy<S, B, E, F>(
+        proxy: Arc<MitmProxy<C>>,
+        req: Request<Incoming>,
+        service: S,
+    ) -> Result<Response<BoxBody<Bytes, E>>, E>
     where
-        S: Fn(Request<Incoming>) -> F,
-        F: Future<Output = Result<Response<B>, E>>,
+        S: Fn(Request<Incoming>) -> F + Send + Clone + 'static,
+        F: Future<Output = Result<Response<B>, E>> + Send,
+        B: Body<Data = Bytes, Error = E> + Send + Sync + 'static,
+        E: std::error::Error + Send + Sync + 'static,
     {
-        service(req).await
+        if req.method() == Method::CONNECT {
+            // https
+            let connect_url = {
+                let mut parts = req.uri().clone().into_parts();
+                parts.scheme = Some(hyper::http::uri::Scheme::HTTPS);
+                parts.path_and_query = Some(hyper::http::uri::PathAndQuery::from_static("/"));
+
+                Uri::from_parts(parts).unwrap()
+            };
+            let Some(connect_authority) = req.uri().authority().cloned() else {
+                tracing::error!(
+                    "Bad CONNECT request: {}, Reason: Invalid Authority",
+                    req.uri()
+                );
+                return Ok(no_body(StatusCode::BAD_REQUEST));
+            };
+
+            tokio::spawn(async move {
+                let Ok(client) = hyper::upgrade::on(req).await else {
+                    tracing::error!(
+                        "Bad CONNECT request: {}, Reason: Invalid Upgrade",
+                        connect_authority
+                    );
+                    return;
+                };
+                if let Some(root_cert) = proxy.root_cert.as_ref() {
+                    let Ok(server_config) =
+                        // Even if URL is modified by middleman, we should sign with original host name to communicate client.
+                        server_config(connect_authority.host().to_string(), root_cert.borrow(), true)
+                    else {
+                        tracing::error!("Failed to create server config for {}", connect_authority.host());
+                        return;
+                    };
+                    // TODO: Cache server_config
+                    let server_config = Arc::new(server_config);
+                    let tls_acceptor = tokio_rustls::TlsAcceptor::from(server_config);
+                    let client = match tls_acceptor.accept(TokioIo::new(client)).await {
+                        Ok(client) => client,
+                        Err(err) => {
+                            tracing::error!(
+                                "Failed to accept TLS connection for {}, {}",
+                                connect_authority.host(),
+                                err
+                            );
+                            return;
+                        }
+                    };
+                    let f = move |mut req: Request<_>| {
+                        let connect_authority = connect_authority.clone();
+                        let service = service.clone();
+
+                        async move {
+                            inject_authority(&mut req, connect_authority.clone());
+                            service(req).await
+                        }
+                    };
+                    let res = if client.get_ref().1.alpn_protocol() == Some(b"h2") {
+                        server::conn::http2::Builder::new(TokioExecutor::new())
+                            .serve_connection(TokioIo::new(client), service_fn(f))
+                            .await
+                    } else {
+                        server::conn::http1::Builder::new()
+                            .preserve_header_case(true)
+                            .title_case_headers(true)
+                            .serve_connection(TokioIo::new(client), service_fn(f))
+                            .with_upgrades()
+                            .await
+                    };
+
+                    if let Err(err) = res {
+                        tracing::error!("Error in proxy: {}", err);
+                    }
+                } else {
+                    let Ok(mut server) = TcpStream::connect(connect_authority.as_str()).await
+                    else {
+                        tracing::error!("Failed to connect to {}", connect_authority);
+                        return;
+                    };
+                    let _ =
+                        tokio::io::copy_bidirectional(&mut TokioIo::new(client), &mut server).await;
+                }
+            });
+
+            Ok(Response::new(
+                http_body_util::Empty::new()
+                    .map_err(|never: std::convert::Infallible| match never {})
+                    .boxed(),
+            ))
+        } else {
+            // http
+            service(req).await.map(|res| res.map(|b| b.boxed()))
+        }
     }
 }
 
@@ -712,7 +810,7 @@ impl<C> MitmProxyImpl<C> {
     }
 }
 
-fn no_body(status: StatusCode) -> Response<BoxBody<Bytes, Arc<hyper::Error>>> {
+fn no_body<E>(status: StatusCode) -> Response<BoxBody<Bytes, E>> {
     let mut res = Response::new(Empty::new().map_err(|never| match never {}).boxed());
     *res.status_mut() = status;
     res
